fad4ed2abb05a8c8e97831b17e09f1aa5ac06531
FrjlGAM <frijmagpulong@gmail.com>
2025-05-11 01:11:55 +0800

    landing design okie

--- src/landing/components/Logo.tsx
+++ src/landing/components/Logo.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/ustp-things-logo.png';
 export default function Logo() {
   return (
     <div className="flex items-center">
-      <img src={logo} alt="USTP Things Logo" className="w-16 h-10 object-contain" />
+      <img src={logo} alt="USTP Things Logo" className="w-[117px] h-[63px] object-contain" />
     </div>
   );
 } 
